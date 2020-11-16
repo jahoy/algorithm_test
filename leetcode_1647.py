6a365546314231d90b7781ba5c86b77f5a3fd815
jahoy <jahoy321@gmail.com>
2020-11-16 23:02:07 +0900

    우선순위 큐

---
+++ leetcode_1647.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+
+from collections import Counter
+import heapq
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        heap = sorted(-x for x in Counter(s).values())
+        heapq.heapify(heap)
+        count = 0 
+        while len(heap) >=2:
+            max_ = heapq.heappop(heap)
+            if max_ == heap[0]:
+                count += 1
+                if max_+1 != 0:
+                    heapq.heappush(heap, max_+1)
+        return count
+        
