eb9ad44d102fa0098283bc994b2d091c5f3407c8
jahoy <jahoy321@gmail.com>
2020-11-29 00:25:02 +0900

    sliding window

---
+++ leetcode_3.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/211683/Python-3-Clean-and-Correct
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        right = 0
+        
+        n = len(s) 
+        memo = set()
+        longgest = 0
+        while left < n and right < n:
+            if s[right] not in memo:
+                memo.add(s[right])
+                longgest = max(longgest, right - left + 1)
+                right +=1
+            else:
+                memo.remove(s[left])
+                left += 1
+        
+        return longgest
+                
+        
+    
+        
