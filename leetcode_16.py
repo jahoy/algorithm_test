e8547c0de9613a15700b3fce8b1dd64a885f12d9
jahoy <jahoy321@gmail.com>
2020-11-29 18:42:49 +0900

    three pointer

---
+++ leetcode_16.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/3sum-closest/
+# https://leetcode.com/problems/3sum-closest/discuss/7871/Python-O(N2)-solution
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        length = len(nums)
+        if length < 3:
+            return
+        nums.sort()
+        
+        result = float('inf')
+        
+        for pointer in range(length - 2):
+            left = pointer+1
+            right = length - 1
+            
+            while left < right:
+                total = nums[pointer] + nums[left] + nums[right]
+                
+                if abs(total - target) == 0:
+                    return total
+                
+                if abs(total - target) < abs(result - target):
+                    result = total
+                
+                if total < target:
+                    left += 1
+                elif total > target:
+                    right -= 1
+                
+        return result
