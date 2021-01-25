4d0b4c589f2aaf00279f5534276276d50067fd8e
jahoy <jahoy321@gmail.com>
2021-01-25 20:59:22 +0900

    [update] practice longest palindrome

--- practice.py
+++ practice.py
@@ -1,13 +1,18 @@
-from collections import Counter
-import heapq
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def helper(l, r):
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
 
-heap = [-x for x in Counter.values()]
-heapq.heapify(heap)
-count = 0
-while len(heap) >= 2:
-    max_  =heapq.heappop(heap)
-    if max_ == heap[0]:
-        count += 1
-        if max_ + 1 != 0:
-            heapq.heappush(heap, max+ +)
+        res = ""
+
+        for i in range(len(s)):
+            test = helper(i,i)
+            if len(test) > len(res): res = test
+            test = helper(i,i+1)
+            if len(test) > len(res): res = test
+        
+        return res
