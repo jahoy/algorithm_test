6774e65890326ba7e55012d24df9c227bfaadf8c
jahoy <jahoy321@gmail.com>
2021-01-27 20:26:57 +0900

    [update] qsort 구현

--- practice.py
+++ practice.py
@@ -1,18 +1,13 @@
+def qsort(data):
+    if len(data) <= 1:
+        return data
+    left, right = list(), list()
+    pivot = data[0]
 
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        def helper(l, r):
-            while(l >= 0 and r < len(s) and s[l] == s[r]):
-                l -= 1
-                r += 1
-            return s[l+1:r]
+    for index in range(1, len(data)):
+        if data[index] < pivot:
+            left.append(data[index])
+        else:
+            right.append(data[index])
 
-        res = ""
-
-        for i in range(len(s)):
-            test = helper(i,i)
-            if len(test) > len(res): res = test
-            test = helper(i,i+1)
-            if len(test) > len(res): res = test
-        
-        return res
+    return qsort(left) + [pivot] + qsort(right)
