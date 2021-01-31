791235d446bdaaf97e481a75ba2b98e1b83b1431
jahoy <jahoy321@gmail.com>
2021-01-31 17:22:43 +0900

    [update] new_sorting algorithm

--- practice.py
+++ practice.py
@@ -1,78 +1,45 @@
-# 퀵정렬
 def qsort(data):
     if len(data) <= 1:
         return data
-    left, right = list(), list()
-    pivot = data[0]
 
-    for index in range(1, len(data)):
-        if data[index] < pivot:
-            left.append(data[index])
+    pivot = data[0]
+    left, right = list(), list()
+
+    for i in range(1, len(data)):
+        if i < pivot:
+            left.append(data[i])
         else:
-            right.append(data[index])
+            right.append(data[i])
 
     return qsort(left) + [pivot] + qsort(right)
 
 
-# 합병정렬
+
 def merge(left, right):
     merged = list()
-    left_index, right_index = 0, 0
-    
-    # case 1: left/right 둘다 있을때
-    while left_index < len(left) and right_index < len(right):
-        if left[left_index] < right[right_index]:
-            merged.append(left[left_index])
-            left_index += 1
+    l, r = 0, 0
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            merged.append(left[l])
+            l += 1
         else:
-            merged.append(right[right_index])
-            right_index += 1
+            merged.append(right[r])
+            r += 1
 
-    # case 2: left 데이터가 없을때
-    while left_index < len(left):
-        merged.append(left[left_index])
-        left_index += 1
-    
-    # case 3: right 데이터가 없을때
-    while right_index < len(right):
-        merged.append(right[right_index])
-        right_index += 1
+    if l < len(left):
+        merged.extend(left[l:])
+    if r < len(right):
+        merged.extend(right[r:])
 
     return merged
 
 
-def mergesplit(data):
+def merge_split(data):
     if len(data) <= 1:
         return data
+    
+    medium = int(len(data) / 2)
 
-    medium = len(data) // 2
-    left = mergesplit(data[:medium])
-    right = mergesplit(data[medium:])
+    left  = merge_split(data[:medium])
+    right = merge_split(data[medium:])
     return merge(left, right)
-
-# 거품 정렬
-def bubble_sort(data):
-    for i in range(len(data) - 1):
-        for index in range(len(data) - i - 1):
-            if data[i] > data[i+1]:
-                data[i], data[i+1] = data[i+1], data[i]
-    return data
-
-
-# 삽입 정렬
-def insertion_sort(data):
-    for i in range(len(data)-1):
-        for index in range(i+1, 0, -1):
-            if data[index] < data[index-1]:
-                data[index], data[index-1] = data[index-1], data[index]
-    return data
-
-
-# 선택 정렬
-def selection_sort(data):
-    for stand in range(len(data)-1):
-        lowest = stand
-        for i in range(stand+1, len(data)):
-            if data[lowest] > data[i]:
-                lowest = i
-        data[lowest], data[stand] = data[stand], data[lowest]
