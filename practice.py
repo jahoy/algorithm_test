11ba29c6e7ffa36d828be535085c5dddec1eb895
jahoy <jahoy321@gmail.com>
2021-01-31 17:33:41 +0900

    [update] longest palindrome

--- practice.py
+++ practice.py
@@ -1,16 +1,18 @@
-def is_palindrome(s:str) -> bool:
-    l, r = 0, len(s)-1
-
-    while l < r:
-        while l < r and not s[l].isalnum():
-            l += 1
-        while l < r and not s[r].isalnum():
-            r -= 1
-
-        if s[l].lower() != s[r].lower():
-            return False
+class Solution:
+    def longest_palindrome(self, s:str) -> str:
+        def helper(l, r):
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
         
-        l += 1
-        r -= 1
+        res  = ""
 
-    return True
+        for i in range(len(s)):
+            test = helper(i,i)
+            if len(test) > len(res): res = test
+
+            test = helper(i,i+1)
+            if len(test) > len(res): res = test
+        
+        return res
