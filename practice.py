8d0c4bf32e9cb05a692b63745b0760a8737a48f3
jahoy <jahoy321@gmail.com>
2021-01-31 17:47:59 +0900

    [update] dp

--- practice.py
+++ practice.py
@@ -1,9 +1,11 @@
-class Solution:
-    def max_subarray(self, nums: List[int]) -> int:
-        total_sum = max_sum = nums[0]
+def rob(nums:List[int])-> int:
+    if not nums: return 0
+    if len(nums) < 3: return max(nums)
 
-        for num in nums[1:]:
-            total_sum = max(total_sum + num, num)
-            max_sum = max(max_sum, total_sum)
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
 
-        return max_sum
+    for i in range(2, len(nums)):
+        dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+    return dp[-1]
