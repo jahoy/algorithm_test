b1c6b5fffe215c9454e3ce39d9740e7503adbbd0
jahoy <jahoy321@gmail.com>
2021-02-28 14:20:04 +0900

    no message

--- practice.py
+++ practice.py
@@ -1,17 +1,14 @@
-# https://leetcode.com/problems/minimum-size-subarray-sum/
-# https://leetcode.com/problems/minimum-size-subarray-sum/discuss/59093/Python-O(n)-and-O(n-log-n)-solution
-
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 class Solution:
-    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
-        left , total = 0, 0
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        numbers = set()
+        missing_nums = []
+        for num in nums:
+            numbers.add(num)
         
-        result = float('inf')
-        for right, num in enumerate(nums):
-            total += nums[right]
-            while total >= s and left <= right:
-                result = min(result, right - left + 1)
-                total -= nums[left]
-                left += 1
+        for i in range(1, len(nums) + 1):
+            if i not in numbers:
+                missing_nums.append(i)
         
-        return result if result <= len(nums) else 0
+        return missing_nums
