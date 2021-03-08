145a12200cca5b88aec3e888ac5cca6787d0e4c7
jahoy <jahoy321@gmail.com>
2021-03-08 21:56:18 +0900

    leetcode update

--- practice.py
+++ practice.py
@@ -1,28 +1,20 @@
-# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
-# https://www.youtube.com/watch?v=iUSXEvV2IUA&feature=youtu.be&ab_channel=BrainRefactor
-# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/discuss/152682/Python-simple-recursive-solution-with-detailed-explanation
+# https://leetcode.com/problems/house-robber/
+# https://www.youtube.com/watch?v=n7TrnGtQz1c&ab_channel=TimothyHChang
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+
+
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root == p or root == q:
-            return root
+    def rob(self, nums: List[int]) -> int:
+        if not nums: return 0
+        if len(nums) < 3: return max(nums)
         
-        left, right = None, None
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         
-        if root.left:
-            left = self.lowestCommonAncestor(root.left, p, q)
-        if root.right:
-            right = self.lowestCommonAncestor(root.right, p, q)
-            
-        if left and right:
-            return root
-        else:
-            return left or right
+        
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return dp[-1]
         
