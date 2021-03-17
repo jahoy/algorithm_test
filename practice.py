654235b12afd07007f1196cdc199daad4dccbd4b
jahoy <jahoy321@gmail.com>
2021-03-17 23:22:06 +0900

    [update] root

--- practice.py
+++ practice.py
@@ -1,17 +1,20 @@
-# https://leetcode.com/problems/minimum-size-subarray-sum/
-# https://leetcode.com/problems/minimum-size-subarray-sum/discuss/59093/Python-O(n)-and-O(n-log-n)-solution
+# https://leetcode.com/problems/house-robber/
+# https://www.youtube.com/watch?v=n7TrnGtQz1c&ab_channel=TimothyHChang
+
+
 
 
 class Solution:
-    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
-        left , total = 0, 0
+    def rob(self, nums: List[int]) -> int:
+        if not nums: return 0
+        if len(nums) < 3: return max(nums)
         
-        result = float('inf')
-        for right, num in enumerate(nums):
-            total += nums[right]
-            while total >= s and left <= right:
-                result = min(result, right - left + 1)
-                total -= nums[left]
-                left += 1
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         
-        return result if result <= len(nums) else 0
+        
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return dp[-1]
+        
