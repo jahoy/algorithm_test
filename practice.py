2ec9323c1eed41adb9f6f7a14e6e53625e993a92
jahoy <jahoy321@gmail.com>
2021-03-18 21:44:47 +0900

    [update] update algorithm

--- practice.py
+++ practice.py
@@ -1,20 +1,34 @@
-# https://leetcode.com/problems/house-robber/
-# https://www.youtube.com/watch?v=n7TrnGtQz1c&ab_channel=TimothyHChang
+# https://www.acmicpc.net/problem/1012
+
+import sys
+sys.setrecursionlimit(50000)
+input = sys.stdin.readline
+
+dx = (-1, 1, 0, 0)
+dy = (0, 0, -1, 1)
 
 
+def dfs(x, y):
+    visited[y][x] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < M and 0 <= ny < N and not visited[ny][nx] and matrix[ny][nx] == 1:
+            dfs(nx, ny)
 
+T = int(input())
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    count = 0
+    matrix = [[0]*50 for _ in range(50)]
+    visited = [[False]*50 for _ in range(50)]
+    for _ in range(K):
+        x, y = map(int, input().split())
+        matrix[y][x] = 1
+    for y in range(N):
+        for x in range(M):
+            if matrix[y][x] == 1 and not visited[y][x]:
+                dfs(x, y)
+                count +=1
+    print(count)
 
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if not nums: return 0
-        if len(nums) < 3: return max(nums)
-        
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        
-        
-        for i in range(2, len(nums)):
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-        return dp[-1]
-        
