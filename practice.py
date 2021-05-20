92bf3b3ca970714137d98a8eb26f0222c3447b2d
jahoy <jahoy321@gmail.com>
2021-05-20 18:49:52 +0900

    interval list intersection

--- practice.py
+++ practice.py
@@ -1,13 +1,19 @@
 class Solution:
-    def merge(self, intervals:List[List[int]]) -> List[List[int]]:
-        if len(intervals) < 2:
-            return intervals
-        intervals.sort()
+    def intervalIntersection(self, A:List[List[int]], B:List[List[int]]) -> List[List[int]]:
+        a = 0
+        b = 0
+        N, M = len(A), len(B)
 
-        output = [intervals[0]]
-        for start, end in intervals[1:]:
-            if output[-1][1] < start:
+        output = []
+        while a < N and b < M:
+            start = max(A[a][0], B[b][0])
+            end = min(A[a][1], B[b][1])
+
+            if start <= end:
                 output.append([start, end])
-            elif start <= output[-1][1] < end:
-                output[-1][1] = end
+
+            if A[a][1] < B[b][1]:
+                a += 1
+            else:
+                b += 1
         return output
