994fbd67f5de0a5aa94dcb686d9dd14e2582c160
jahoy <jahoy321@gmail.com>
2021-05-21 17:55:19 +0900

    numsubarrayproduct lessthan k

--- practice.py
+++ practice.py
@@ -1,28 +1,17 @@
 class Solution:
-    def threeSumClosest(self, nums:List[int], target:int) -> int:
+    def numSubarrayProductLessThanK(self, nums:List[int], k:int) -> int:
+        left = 0
         length = len(nums)
-        if length < 3:
-            return
-        nums.sort()
+        product = 1
 
-        result = float('inf')
+        result = 0
+        for right in range(length):
+            product *= nums[right]
 
-        for pointer in range(length - 2):
-            left = pointer + 1
-            right = length - 1
-
-            while left < right:
-                total = nums[pointer] + nums[left] + nums[right]
-
-                if abs(total - target) == 0:
-                    return total
-                
-                if abs(total - target) < abs(result - target):
-                    result = total
-                
-                if total < target:
-                    left += 1 
-                elif total > target:
-                    right -= 1
+            while product >= k and left <= right:
+                product = product // nums[left]
+                left += 1
+            
+            result += right - left + 1
 
         return result
