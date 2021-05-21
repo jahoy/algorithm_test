fedbd28f3581159e0a4874a11ee9e4b8ce9e6a3d
jahoy <jahoy321@gmail.com>
2021-05-21 17:05:10 +0900

    threeSum

--- practice.py
+++ practice.py
@@ -1,15 +1,30 @@
 class Solution:
-    def mergeTwoLists(self, l1:ListNode, l2: ListNode) -> ListNode:
-        head = curr = ListNode(None)
+    def threeSum(self, nums:List[int])-> List[List[int]]:
+        length = len(nums)
+        output = []
+        nums.sort()
 
-        while l1 != None and l2 != None:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
+        for pointer in range(length - 1):
+            if pointer > 0 and nums[pointer] == nums[pointer -1]:
+                continue
 
-            curr = curr.next
-        curr.next = l1 or l2
-        return head.next
+            left = pointer + 1
+            right = length - 1
+
+            while left < right:
+                total = nums[pointer] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                elif total == 0:
+                    output.append([nums[pointer], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+            return output
