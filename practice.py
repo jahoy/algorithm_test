2b34810844ae9e9c96e36d04334bc4a217f31483
jahoy <jahoy321@gmail.com>
2021-05-21 12:49:40 +0900

    movezeros

--- practice.py
+++ practice.py
@@ -1,8 +1,11 @@
 class Solution:
-    def middleNode(self, head:ListNode) -> ListNode:
-        rabbit, turtle = head, head
-
-        while rabbit and rabbit.next:
-            turtle = turtle.next
-            rabbit = rabbit.next.next
-        return turtle
+    def moveZeros(self, nums:List[int]) -> None:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+            
+            if nums[slow] != 0:
+                slow += 1
+        
+        return nums
