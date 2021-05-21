d4f3353e3e10d7e73664aae732aed2c0047f29c0
jahoy <jahoy321@gmail.com>
2021-05-21 12:41:02 +0900

    middleNode

--- practice.py
+++ practice.py
@@ -1,19 +1,8 @@
 class Solution:
-    def intervalIntersection(self, A:List[List[int]], B:List[List[int]]) -> List[List[int]]:
-        a = 0
-        b = 0
-        N, M = len(A), len(B)
+    def middleNode(self, head:ListNode) -> ListNode:
+        rabbit, turtle = head, head
 
-        output = []
-        while a < N and b < M:
-            start = max(A[a][0], B[b][0])
-            end = min(A[a][1], B[b][1])
-
-            if start <= end:
-                output.append([start, end])
-
-            if A[a][1] < B[b][1]:
-                a += 1
-            else:
-                b += 1
-        return output
+        while rabbit and rabbit.next:
+            turtle = turtle.next
+            rabbit = rabbit.next.next
+        return turtle
